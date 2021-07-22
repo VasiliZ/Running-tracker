2ea7fb3bb44a592ca0ee74b44445a545234f5dec
Vasili.Zaitsev <zaicev.vasili@gmail.com>
2021-07-22 17:20:39 +0300

    add header drawer and toolbar

--- app/src/main/java/com/github/rtyvz/senla/tr/runningtracker/extension/ContextExt.kt
+++ app/src/main/java/com/github/rtyvz/senla/tr/runningtracker/extension/ContextExt.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 
 private const val RUNNING_SHARED_PREFERENCE_STORAGE = "RUNNING_SHARED_PREFERENCE_STORAGE"
+
 fun Context.getSharedPreference(): SharedPreferences {
     return this.getSharedPreferences(RUNNING_SHARED_PREFERENCE_STORAGE, Context.MODE_PRIVATE)
 }
--- app/src/main/java/com/github/rtyvz/senla/tr/runningtracker/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/rtyvz/senla/tr/runningtracker/ui/main/MainActivity.kt
@@ -1,10 +1,63 @@
 package com.github.rtyvz.senla.tr.runningtracker.ui.main
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.github.rtyvz.senla.tr.runningtracker.R
+import com.github.rtyvz.senla.tr.runningtracker.entity.ui.UserData
+import com.github.rtyvz.senla.tr.runningtracker.extension.getSharedPreference
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val USER_TOKEN = "USER_TOKEN"
+        private const val USER_NAME = "USER_NAME"
+        private const val USER_LAST_NAME = "USER_LAST_NAME"
+        private const val USER_EMAIL = "USER_EMAIL"
+        private const val EMPTY_STRING = ""
+    }
+
+    private lateinit var userData: UserData
+    private lateinit var toolBar: Toolbar
+    private lateinit var navHeaderUserNameTextView: MaterialTextView
+    private lateinit var navHeaderUserEmailTextView: MaterialTextView
+    private lateinit var navigationView: NavigationView
+    private lateinit var headerNavView: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViews()
+        getUserDataFromPrefs()
+        setSupportActionBar(toolBar)
+        setDataToNavHeader()
+    }
+
+    private fun findViews() {
+        toolBar = findViewById(R.id.toolBar)
+        navigationView = findViewById(R.id.navigationView)
+        headerNavView = navigationView.getHeaderView(0)
+        navHeaderUserEmailTextView = headerNavView.findViewById(R.id.userEmailTextView)
+        navHeaderUserNameTextView = headerNavView.findViewById(R.id.userNameTextView)
+
+    }
+
+    private fun getUserDataFromPrefs() {
+        val prefs = this.getSharedPreference()
+        userData = UserData(
+            prefs.getString(USER_TOKEN, EMPTY_STRING) ?: EMPTY_STRING,
+            prefs.getString(USER_NAME, EMPTY_STRING) ?: EMPTY_STRING,
+            prefs.getString(USER_LAST_NAME, EMPTY_STRING) ?: EMPTY_STRING,
+            prefs.getString(USER_EMAIL, EMPTY_STRING) ?: EMPTY_STRING
+        )
+    }
+
+    private fun setDataToNavHeader() {
+        navHeaderUserEmailTextView.text = userData.email
+        navHeaderUserNameTextView.text = userData.name
     }
 }
