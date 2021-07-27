3a658e9dd32a5024b0477669ef9fb4d7e5fb0240
Vasili.Zaitsev <zaicev.vasili@gmail.com>
2021-07-27 12:13:30 +0300

    fix on finish click dialog

--- app/src/main/java/com/github/rtyvz/senla/tr/runningtracker/ui/running/ClickFinishButtonDialog.kt
+++ app/src/main/java/com/github/rtyvz/senla/tr/runningtracker/ui/running/ClickFinishButtonDialog.kt
@@ -17,7 +17,7 @@ class ClickFinishButtonDialog : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return AlertDialog.Builder(requireContext())
-            .setTitle(getString(R.string.running_activity_dialog_finish_button_is_not_click_yet))
+            .setTitle(getString(R.string.running_activity_dialog_confirm_click_finish_button))
             .setPositiveButton(getString(R.string.running_activity_dialog_ok_button)) { dialog, _ ->
                 dialog.dismiss()
             }.create()
