5dd116950b37a4b0baa2a64e6bd904aa60eede3f
Vasili.Zaitsev <zaicev.vasili@gmail.com>
2021-08-16 11:57:19 +0300

    fix endpoint

--- app/src/main/java/com/github/rtyvz/senla/tr/runningtracker/network/Endpoint.kt
+++ app/src/main/java/com/github/rtyvz/senla/tr/runningtracker/network/Endpoint.kt
@@ -1,9 +1,10 @@
 package com.github.rtyvz.senla.tr.runningtracker.network
 
 object Endpoint {
-    const val REGISTER_USER = "lesson-26.php?method=register"
-    const val LOGIN_USER = "lesson-26.php?method=login"
-    const val GET_USER_TRACKS = "lesson-26.php?method=tracks"
-    const val SAVE_USER_TRACK = "lesson-26.php?method=save"
-    const val GET_TRACK_POINTS = "lesson-26.php?method=points"
+    private const val BASE = "lesson-26.php?method="
+    const val REGISTER_USER = "${BASE}register"
+    const val LOGIN_USER = "${BASE}login"
+    const val GET_USER_TRACKS = "${BASE}tracks"
+    const val SAVE_USER_TRACK = "${BASE}save"
+    const val GET_TRACK_POINTS = "${BASE}points"
 }
